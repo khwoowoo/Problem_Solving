00e046ff50d7e083a5483c3f0671e321318d65c2
Hyunwoo Kang <gusdn741021@gmail.com>
2023-11-19 16:47:14 +0900

    Update 10815.py

--- baejoon/이분 탐색/10815.py
+++ baejoon/이분 탐색/10815.py
@@ -14,3 +14,19 @@ for i in range(M):
         answer[i] = 1
 
 print(' '.join(map(str, answer)))  # 간단한 출력
+
+# 모범 답안
+from bisect import bisect_left, bisect_right
+N = int(input())
+cards = sorted(map(int, input().split()))
+M = int(input())
+qry = list(map(int, input().split()))
+ans = []
+
+for q in qry:
+    l = bisect_left(cards, q)
+    r = bisect_right(cards, q)
+    ans.append(i if r - l else 0)
+
+print(*ans)
+#print(' '.join(map(str, aswer)))
