1f31cb1252769b5278b26432f5df33d3b2ff18d0
Hyunwoo Kang <gusdn741021@gmail.com>
2023-11-19 19:23:03 +0900

    Update 1018.py

--- baejoon/완전탐색/1018.py
+++ baejoon/완전탐색/1018.py
@@ -19,3 +19,27 @@ for i in range(N - 7):
         candidates.append(check(i, j, False))  # 체스판이 검은색으로 시작하는 경우
 
 print(min(candidates))
+
+# 모범답안
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+ans = 64
+
+def fill(y, x, bw):
+    global ans
+    cnt = 0
+    for i in range(8):
+        for j in range(8):
+            if (i + j) % 2:
+                if board[y + i][x + j] == bw:
+                    cnt +=1
+                else:
+                if board[y + i][x + j] == bw:
+                    cnt +=1
+    ans = min(ans, cnt)
+
+for i in range(N - 7):
+    for j in range(M - 7):
+        fill(i, j, 'W')
+        fill(i, j, 'B')
+print(aws)
