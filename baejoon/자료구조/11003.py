c74e550c20b3236f4c23780b17ef73ff45812467
Hyunwoo Kang <gusdn741021@gmail.com>
2024-03-04 22:13:27 +0900

    Update 11003.py

--- baejoon/자료구조/11003.py
+++ baejoon/자료구조/11003.py
@@ -33,3 +33,22 @@ for i in range(1, N + 1):
   # 결국 해결 방법을 보고 직접 구현했는데 덱을 사용하는 방법있었다...
   # 접근 방식은 비슷했으나 덱 자료구조를 생각을 못 했다...
   # 결국 O(N)의 시간복잡도를 가진다
+  # 아래는 모범 코드. 나는 파이썬 언어를 잘 몰라서 복잡한 코드를 작성한듯..!
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+N, L = map(int, input().split(' '))
+mydeque = deque()
+now = list(map(int,  input().split(' ')))
+
+for i in range(N):
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i], i))
+    
+    if mydeque[0][1] <= i - L:
+        mydeque.popleft()
+    print(mydeque[0][0], end=' ')
+
