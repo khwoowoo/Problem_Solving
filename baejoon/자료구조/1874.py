523e3db7ae7d5c9b239f334252ca980d07e543b7
Hyunwoo Kang <gusdn741021@gmail.com>
2024-03-11 21:20:20 +0900

    [1874.py]: 리팩토링

--- baejoon/자료구조/1874.py
+++ baejoon/자료구조/1874.py
@@ -3,35 +3,23 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = []
+A = [int(input()) for _ in range(N)]  # 리스트 컴프리헨션 사용
 stack = []
-idx = 0
-i = 1
 result = []
+idx = 0
 
-for _ in range(N):
-    A.append(int(input()))
-    
 for i in range(1, N + 1):
     stack.append(i)
     result.append('+')
-    
-    while stack:
-        if stack[-1] == A[idx]:
-            stack.pop()
-            idx += 1
-            result.append('-')
-        else: break
 
-while stack:
-    if stack[-1] == A[idx]:
+    # 스택의 마지막 요소가 A[idx]와 같지 않을 때까지 반복
+    while stack and stack[-1] == A[idx]:
         stack.pop()
         result.append('-')
         idx += 1
-    else: break
 
-
-if len(stack) == 0:
-    for i in result: print(i)
+# 스택이 비어있는지 확인
+if not stack:
+    print('\n'.join(result))  # 결과를 한 번에 출력
 else:
     print("NO")
