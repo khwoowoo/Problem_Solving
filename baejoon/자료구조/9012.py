d11ff9d29e9ee1bb65be5a7b3d86ca0018f66bb8
Hyunwoo Kang <gusdn741021@gmail.com>
2024-03-11 20:10:58 +0900

    Update 9012.py

--- baejoon/자료구조/9012.py
+++ baejoon/자료구조/9012.py
@@ -1,3 +1,4 @@
+# 숫자를 이용한 방법
 times = int(input())
 
 def VPS(data):
@@ -13,3 +14,19 @@ def VPS(data):
 
 for data in range(times):
   print(VPS(input()))
+
+# 스택을 이용한 방법
+N = int(input())
+stack = []
+
+for _ in range(N):
+    paren = input()
+    isFailed = False
+    stack.clear()
+    for item in paren:
+        if item == '(': stack.append(item)
+        else: 
+            if len(stack) > 0: stack.pop()
+            else: isFailed = True
+    if isFailed == False and len(stack) == 0: print("YES")
+    else: print("NO")
