8a3e88c84a33284959736390d636d47e2063d3e1
Hyunwoo Kang <gusdn741021@gmail.com>
2024-04-05 21:57:10 +0900

    Update [java][2018]수들의합5.java

--- baejoon/투 포인터/[java][2018]수들의합5.java
+++ baejoon/투 포인터/[java][2018]수들의합5.java
@@ -7,10 +7,8 @@ class Main {
     public static void main(String[] args) throws IOException  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int target = Integer.parseInt(br.readLine());
-        int start= 1;
-        int end = 1;
-        int sum = 1;
-        int count = 1;
+        int start = 1, end = 1, sum = 1, count = 1;
+
         int[] A = new int[target + 1];
         for(int i = 1; i < target + 1; i++) A[i] = i;
 
