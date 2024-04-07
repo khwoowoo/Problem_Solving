089e37e1438ae11880fc99dce45bfa89d7e8deb2
Hyunwoo Kang <gusdn741021@gmail.com>
2024-04-07 19:46:44 +0900

    Update [java][11286]절댓값힙.java

--- baejoon/자료구조/[java][11286]절댓값힙.java
+++ baejoon/자료구조/[java][11286]절댓값힙.java
@@ -23,16 +23,12 @@ class Main {
         for(int i = 0; i < N; i++) {
             int x = Integer.parseInt(br.readLine());
 
-            if (x != 0) {
-                // x가 0이 아닐 때는 (절댓값, 원래 값)을 큐에 추가
-                queue.offer(new SimpleEntry<>(Math.abs(x), x));
-            } else {
+            // x가 0이 아닐 때는 (절댓값, 원래 값)을 큐에 추가
+            if (x != 0) { queue.offer(new SimpleEntry<>(Math.abs(x), x));} 
+            else {
                 // x가 0일 때 큐가 비어있으면 0 출력, 그렇지 않으면 최소 절댓값을 가진 원소 출력 후 제거
-                if (queue.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(queue.poll().getValue());
-                }
+                if (queue.isEmpty()) {System.out.println(0);} 
+                else { System.out.println(queue.poll().getValue());}
             }
         }
     }
