b8925f3eff15a0a6028e1b38de5777ff10f75c38
Hyunwoo Kang <gusdn741021@gmail.com>
2024-05-20 20:32:37 +0900

    Update [java][1940]주몽.java

--- baejoon/투 포인터, 슬라이딩 윈도우/[java][1940]주몽.java
+++ baejoon/투 포인터, 슬라이딩 윈도우/[java][1940]주몽.java
@@ -2,39 +2,56 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// 투 포인터로 구현
+// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException{
-        //입력
-        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
-        int N, M;
-        int[] A;
-        int count = 0;
-        N = Integer.parseInt(br.readLine().trim());
-        M = Integer.parseInt(br.readLine().trim());
-        int start = 0, end = N - 1;
-        A = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+        int start = 0, end = N - 1, count = 0;
+            
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
 
         Arrays.sort(A);
-        while (start < end){
+        
+        while (start < end) {
             int cur = A[start] + A[end];
-            if (cur == M) {
-                count++;
-                start++;
-                end--;
-            }
+        
+            if(cur == M) {count++; start++; end--;}
             else if(cur < M) start++;
             else end--;
-            
         }
         System.out.println(count);
     }
 }
 
 /*
+2초
+더 해서 M를 찾는 문제
+이거는 정렬하고 투포인터를 이용한 방법을 사용하면 될듯
 1 2 3 4 5 7
+1 7
+2 7 -> 찾음
+3 5
+4 5
+
+더한 숫자가 M보다 작으면 앞에 이동
+더한 숫자가 M보다 크면 뒤에서 이동
+
+N, M
+A(정렬)
+start, end
+count
+
+while (start < end) {
+    cur = A[start] + A[end]
+
+    if(cur == M) count++; start++; end++;
+    else if(cur < M) start++;
+    else end++;
+}
 */
 
 
