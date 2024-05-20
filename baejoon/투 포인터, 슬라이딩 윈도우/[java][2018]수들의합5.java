37ffef7c00d2a0e2a76393ebd5ea34e2ff59edb1
Hyunwoo Kang <gusdn741021@gmail.com>
2024-05-20 19:42:04 +0900

    Update [java][2018]수들의합5.java

--- baejoon/투 포인터, 슬라이딩 윈도우/[java][2018]수들의합5.java
+++ baejoon/투 포인터, 슬라이딩 윈도우/[java][2018]수들의합5.java
@@ -4,19 +4,24 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    public static void main(String[] args) throws IOException  {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = Integer.parseInt(br.readLine());
-        int start = 1, end = 1, sum = 1, count = 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N + 1];
+        int start = 1;
+        int end = 1;
+        int count = 0;
 
-        int[] A = new int[target + 1];
-        for(int i = 1; i < target + 1; i++) A[i] = i;
-
-        while(start < target && end < target){
-            if(sum == target) {count += 1; sum -= A[start]; start += 1; }
-            else if(sum < target) {end += 1; sum += A[end];}
-            else if(sum > target) {sum -= A[start]; start += 1; }
+        for(int i = 0; i < N + 1; i++) A[i] = i;
+        
+        int sum = A[start];
+        while(start <= end){
+            if (sum == N){ count++;  sum -= A[start++]; }
+            else if(sum < N){ sum += A[++end];}
+            else{sum -= A[start++];}
+             // System.out.println(String.format("%d %d", start, end));
         }
+
         System.out.println(count);
     }
 }
