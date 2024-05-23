8fb473550e356a820a5e2bd37e14a3882cabb8c0
Hyunwoo Kang <gusdn741021@gmail.com>
2024-05-23 18:40:34 +0900

    Update [java][2178]미로탐색.java

--- baejoon/DFS, BFS, 백트래킹/[java][2178]미로탐색.java
+++ baejoon/DFS, BFS, 백트래킹/[java][2178]미로탐색.java
@@ -1,3 +1,72 @@
+//다시 풀어보기
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int A[][];
+    static int N, M;
+    static int moveX[] = {0, 1, -1, 0};
+    static int moveY[] = {1, 0, 0, -1};
+
+    static class Node{
+        int x;
+        int y;
+        int z;
+
+        Node(int x, int y, int z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    public static boolean isRange(int x, int y){
+        return (0 <= x && x <N && 0 <= y && y < M);
+    }
+
+    public static int BFS(){
+        A[0][0] = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 0, 1));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            for(int i = 0 ; i < 4 ; i++){
+                int x = node.x + moveX[i];
+                int y = node.y + moveY[i];
+                
+                if(x == N -1 && y == M -1) {
+                    return node.z + 1;
+                }                     
+                else if(isRange(x, y) && A[x][y] == 1){
+                    queue.add(new Node(x, y, node.z + 1));
+                    A[x][y] = 0;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M];
+
+        for(int i = 0; i < N; i++){
+            String temp = br.readLine();
+            for(int j = 0; j < M; j++){
+                A[i][j] = temp.charAt(j) - '0';
+            }
+        }
+        System.out.print(BFS());
+    }
+}
+
 import java.util.*;
 import java.io.*;
 
